84414c0902fc9c226b32968d8eb9a2c78a58f6ed
Manju8722 <manjuna8722@gmail.com>
2025-04-05 09:41:37 +0530

    changed  backend url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://mern-auth-server-0zi8.onrender.com:4000', // Redirect everything to port 4000
+        target: 'https://mern-auth-server-0zi8.onrender.com', // Redirect everything to port 4000
         changeOrigin: true,
         secure: false,
       },
